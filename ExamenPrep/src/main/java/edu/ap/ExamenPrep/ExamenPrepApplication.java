be2dc3681b7964a8fcc7bb3abb00c4cef43b8ced
SilkeHenderickx <silke.henderickx@student.ap.be>
2018-08-29 16:44:59 +0200

    Spring Initializer JPA Mysql H2

---
+++ ExamenPrep/src/main/java/edu/ap/ExamenPrep/ExamenPrepApplication.java
@@ -0,0 +1,12 @@
+package edu.ap.ExamenPrep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenPrepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenPrepApplication.class, args);
+	}
+}
