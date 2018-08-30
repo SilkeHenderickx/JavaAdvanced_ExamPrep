e33b3da1c57fa27dde6a2ea0922eb5188ac6b578
SilkeHenderickx <silke.henderickx@student.ap.be>
2018-08-30 11:16:47 +0200

    Book Entity

---
+++ ExamenPrep/src/main/java/edu/ap/ExamenPrep/Book.java
@@ -0,0 +1,60 @@
+package edu.ap.ExamenPrep;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title", unique = true)
+    private String title;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "onHold")
+    private Integer onHold;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.onHold = 0;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Integer getOnHold() {
+        return onHold;
+    }
+
+    public void setOnHold(Integer onHold) {
+        this.onHold = onHold;
+    }
+}
