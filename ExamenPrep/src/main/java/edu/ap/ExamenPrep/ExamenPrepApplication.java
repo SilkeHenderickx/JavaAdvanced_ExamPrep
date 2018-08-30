6a6ecbdc0cf4f728ec729d1c270d305bd98c59e2
SilkeHenderickx <silke.henderickx@student.ap.be>
2018-08-30 16:38:02 +0200

    Removed broken code (reason for not working, unknown)

--- ExamenPrep/src/main/java/edu/ap/ExamenPrep/ExamenPrepApplication.java
+++ ExamenPrep/src/main/java/edu/ap/ExamenPrep/ExamenPrepApplication.java
@@ -1,7 +1,5 @@
 package edu.ap.ExamenPrep;
 
-import edu.ap.ExamenPrep.model.BookRecommender;
-import edu.ap.ExamenPrep.persistence.BookCategory;
 import edu.ap.ExamenPrep.view.UI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -23,21 +21,6 @@ public class ExamenPrepApplication {
 		};
 	}
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExamenPrepApplication.class, args);
-
-			BookRecommender r = new BookRecommender();
-
-			r.AddBook("Harry Potter and the Chamber of Secrets", "Rowling", BookCategory.FICTION);
-			r.AddBook("The Subtle Art of Not Giving a Fuck", "Manson", BookCategory.NONFICTION);
-			r.AddBook("The Gift", "Croggon", BookCategory.FICTION);
-			r.AddBook("Baking", "someChef", BookCategory.NONFICTION);
-
-			System.out.println(r.RecommendBook(null).getTitle());
-			System.out.println(r.RecommendBook(null).getTitle());
-			System.out.println(r.RecommendBook(null).getTitle());
-			System.out.println(r.RecommendBook(null).getTitle());
-			System.out.println(r.RecommendBook(null).getTitle());
-		}
+	public static void main(String[] args) { SpringApplication.run(ExamenPrepApplication.class, args); }
 
 }
